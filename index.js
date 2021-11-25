b8a1e866591c3c13250ee3a091c3053c016b125a
罗雨凤 <1443724970@qq.com>
2021-11-25 09:34:51 +0800

    commit

--- index.js
+++ index.js
@@ -1,33 +1,76 @@
 // 测试JSON.stringify
 function testJSONStringify() {
-    const target = { a: 1, b: 2 };
-    const source = { b: 4, c: 5 };
-    const obj = {
-        name: function () {// 丢失
-            target.a = 2;
-        },
-        name: 1,
-        age: 18
-    };
-    let _obj = JSON.stringify(obj);
+  const target = { a: 1, b: 2 };
+  const source = { b: 4, c: 5 };
+  const obj = {
+    name: function () {
+      // 丢失
+      target.a = 2;
+    },
+    name: 1,
+    age: 18,
+  };
+  let _obj = JSON.stringify(obj);
 
-    console.log(_obj);
+  console.log(_obj);
 }
+
 // 测试JSON.stringify的参数
 function testJSONStringifyParams() {
-    var data = {
-        info: {
-            age: 18,
-            sex: "male"
-        },
-        name: "niuzai",
-    };
+  var data = {
+    info: {
+      age: 18,
+      sex: "male",
+    },
+    name: "niuzai",
+  };
 
-    JSON.stringify(data, function (key, val) {// 过滤函数一层层遍历属性，第一层是整个对象
-        console.log("key is %s", key);
-        console.log("val is %s", typeof (val));
-        return val
-    });
+  JSON.stringify(data, function (key, val) {
+    // 过滤函数一层层遍历属性，第一层是整个对象
+    console.log("key is %s", key);
+    console.log("val is %s", typeof val);
+    return val;
+  });
+}
+
+// 数组过滤 过滤出合法的属性（id）数组
+function filterIdArr() {
+  let list = [];
+  for (let i = 0; i < 20; i++) {
+    list.push({ id: i, status: i % 5 });
+  }
+  let arr = [2, 5, 7, 8, 9, 17, 18, 12];
+  console.log(list);
+  console.log(arr);
+  let result = arr.filter((id) => {
+    let item = list.find((item) => item.id == id);
+    return !(item.status == 3|| item.status == 4)
+  });
+  console.log(result);
+}
+
+// 数组过滤 过滤出合法的list数组
+function filterList() {
+  let list = [];
+  for (let i = 0; i < 20; i++) {
+    list.push({ id: i, status: i % 5 });
+  }
+
+  let arr = [2, 5, 7, 8, 9, 17, 18, 12];
+  let result = list.filter((listitem) => {
+    if (arr.some((item) => listitem.id == item)) {
+      return listitem.status < 3;
+    }
+  });
+
+  let result_1 = [];
+  arr.forEach((id) => {
+    let item = list.find((item) => item.id == id);
+    item.status < 3 && result_1.push(item);
+  });
+
+  console.log(result);
+  console.log(result_1);
 }
 
 // 私有属性的类
@@ -42,27 +85,5 @@ function testJSONStringifyParams() {
 // testJSONStringify()
 // testJSONStringifyParams()
 
-function findBacth() {
-    let list = []
-    for(let i= 0;i<20;i++){
-        list.push({id:i,status:i%5})
-    }
-    console.log(list)
-    // 有一个数组，对应id
-    let arr = [2,5,7,8,9,17,18,12]
-    let result = arr.filter(item=>{
-        let listitem = list.find(listItem=>{
-            return (listItem.id == item)
-        })
-        if(listitem.status == 3 || listitem.status == 4){
-            return false
-        }else{
-            return true
-        }
-    })
-    console.log(result)
-}
-
-findBacth()
-
-
+filterIdArr();
+filterList();
